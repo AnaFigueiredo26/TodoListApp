f235eb8559abbf19671bdafcb79a8274f3728602
AnaFigueiredo26 <af.anafigueiredo@gmail.com>
2024-10-08 19:20:47 +0100

    feat: add filters

--- vue-todo-app/src/App.vue
+++ vue-todo-app/src/App.vue
@@ -1,14 +1,31 @@
 <script setup>
-import { ref, watchEffect } from 'vue'
+import { ref, computed, watchEffect } from 'vue'
 import { nanoid } from 'nanoid'
+
 import ToDoItem from './components/ToDoItem.vue'
 import ToDoForm from './components/ToDoForm.vue'
 import { DATA } from './data'
 
 const STORAGE_KEY = 'vue-tasks'
+
+const filters = {
+  all: (todos) => todos,
+  active: (todos) => todos.filter((todo) => !todo.completed),
+  completed: (todos) => todos.filter((todo) => todo.completed)
+}
+
 const storedTodos = localStorage.getItem(STORAGE_KEY)
 const todos = ref(storedTodos ? JSON.parse(storedTodos) : DATA)
+const visibility = ref('all')
 
+// derived state
+const filteredTodos = computed(() => filters[visibility.value](todos.value))
+
+// handle routing
+window.addEventListener('hashchange', onHashChange)
+onHashChange()
+
+// persist state
 watchEffect(() => {
   localStorage.setItem(STORAGE_KEY, JSON.stringify(todos.value))
 })
@@ -35,14 +52,35 @@ function itemEdited(toDoId, newLabel) {
     toDoToEdit.name = newLabel
   }
 }
+
+function onHashChange() {
+  const route = window.location.hash.replace(/#\/?/, '')
+  if (filters[route]) {
+    visibility.value = route
+  } else {
+    window.location.hash = ''
+    visibility.value = 'all'
+  }
+}
 </script>
 
 <template>
   <div id="app">
     <h1>To-Do List</h1>
     <ToDoForm @addToDo="handleAddToDo" />
+    <ul class="filters">
+      <li>
+        <a href="#/all" :class="{ selected: visibility === 'all' }">All</a>
+      </li>
+      <li>
+        <a href="#/active" :class="{ selected: visibility === 'active' }">Active</a>
+      </li>
+      <li>
+        <a href="#/completed" :class="{ selected: visibility === 'completed' }">Completed</a>
+      </li>
+    </ul>
     <ul>
-      <li v-for="todo in todos" :key="todo.id">
+      <li v-for="todo in filteredTodos" :key="todo.id">
         <ToDoItem
           :name="todo.name"
           :completed="todo.completed"
